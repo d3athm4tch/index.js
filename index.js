da47d2e79db741cd64ab1cebead7bb2ac8b4af64
d3athm4tch <114290030+d3athm4tch@users.noreply.github.com>
2022-09-24 14:03:53 +0300

    index.js
    website itself

---
+++ index.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:#0e1111">
+
+<h3 style="text-align:center; color: aliceblue;">Website in development</h1>
+<p style="text-align:center; color: aliceblue;">We're working on it.</p>
+
+</body>
+</html>
